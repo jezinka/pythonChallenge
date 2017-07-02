0c8dde66074852f1cc14c994c9716720f304f8df
jezinka <jezina1989@gmail.com>
2017-07-02 18:32:23 +0200

    using numpy roll

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
 # http://www.pythonchallenge.com/pc/def/map.html
 
 import string
+from numpy import roll
+
 letters = string.lowercase
-trans = string.maketrans(letters, letters[2::] + letters[0:2])
+transition = ''.join(roll(list(letters), -2))
+
+trans = string.maketrans(letters, transition)
 
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 print text.translate(trans)
